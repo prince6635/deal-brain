61e81dced1cb843263969cdae64e55e0a7488ea2
Zi Wang <ziwang@uber.com>
2016-09-27 21:40:35 -0700

    Bootstrap express service

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# deal-brain
+# Deal Brain
+Subscribe customized deal and get alerts when it's available in any place.
+
+## Features
+* Web scraping
+* Search
+* Data analysis
+* Push notification
+
+## Components
+* Node.js (express)
+* MongoDB
+* ElasticSearch
+
+## References
---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function (req, res) {
+    res.json("Here's the deal bran.");
+});
+
+app.get('/health', function (req, res) {
+    res.json('Ok');
+});
+
+app.listen(3000, function (err) {
+    if (err) throw err;
+    console.log("Server is running on port 3000");
+});
